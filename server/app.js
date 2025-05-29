fd727d03dd59c2e32faddc69e90eaf685914c642
yazanalhayek <yazzhayek1998@gmail.com>
2025-05-29 22:49:18 +0200

    dependencies fixes

--- server/app.js
+++ server/app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const { initializeSocket } = require('./infra/socket');
+const {join} = require("node:path");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 const app = express();
 const server = http.createServer(app);
 initializeSocket(server);
 
+// Serve static files from React build
+app.use(express.static(join(__dirname, '../client/build')));
+
+// Fallback naar index.html voor React Router
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
