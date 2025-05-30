1021bea1bb4bc8a6b6d97dedfaac487012b90baa
yazanalhayek <yazzhayek1998@gmail.com>
2025-05-30 12:14:27 +0200

    ongebruikelijk stukje code verwijderen

--- server/app.js
+++ server/app.js
@@ -8,12 +8,6 @@ const app = express();
 const server = http.createServer(app);
 initializeSocket(server);
 
-// Serve static files from React build
-app.use(express.static(join(__dirname, '../client/build')));
 
-// Fallback naar index.html voor React Router
-app.get('*', (req, res) => {
-    res.sendFile(join(__dirname, '../client/build/index.html'));
-});
 
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
