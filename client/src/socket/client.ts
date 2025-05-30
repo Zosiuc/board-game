b50d7233b1518c05cbc9ea0553d3724a2d8daf31
yazanalhayek <yazzhayek1998@gmail.com>
2025-05-30 12:32:16 +0200

    --lagacy-peer-deps

--- client/src/socket/client.ts
+++ client/src/socket/client.ts
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 
+
 //export const socket = io("http://localhost:3001");
 
 // VOOR DEPLOYMENT
-export const socket = io('https://board-game-zmco.onrender.com')
+export const socket = io("https://board-game-zmco.onrender.com");
